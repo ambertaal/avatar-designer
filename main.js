5ecc204b16020e6982e525d3b3ecee8febdb1263
github-ambertaal <amber.taal@gmail.com>
2023-10-09 12:08:52 +0200

    refactored code

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
-import "./style.css";
-
 const myRange = document.getElementById("myRange");
 const mySize = document.getElementById("size");
 const image = document.querySelector(".image");
 const square = document.getElementById("square");
+const imageContainer = document.getElementById("image-container");
 
-size.addEventListener("change", (event) => {
-  console.log(event.target.value);
-  image.style.width = `${parseInt(event.target.value, 10)}px`;
-  image.style.height = `${parseInt(event.target.value, 10)}px`;
+mySize.addEventListener("input", (event) => {
+  imageContainer.style.height = `${parseInt(event.target.value, 10)}px`;
 });
 
-myRange.addEventListener("change", (event) => {
-  image.style.borderRadius = `${parseInt(event.target.value, 10)}px`;
+myRange.addEventListener("input", (event) => {
+  imageContainer.style.borderRadius = `${parseInt(event.target.value, 10)}%`;
 });
 
 const favColor = document.getElementById("favcolor");
 
-favColor.addEventListener("change", (event) => {
-  image.style.borderColor = event.target.value;
+favColor.addEventListener("input", (event) => {
+  imageContainer.style.borderColor = event.target.value;
 });
 
 const buttons = document.querySelectorAll(".button");
@@ -26,6 +23,5 @@ const buttons = document.querySelectorAll(".button");
 buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     image.src = event.target.dataset.imagePath;
-    image.parentElement.className = "show";
   });
 });
